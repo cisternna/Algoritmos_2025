24401ac3b4695f17e06c8b93409578b95ace8f07
cisternna <105678565+cisternna@users.noreply.github.com>
2025-05-23 10:21:12 -0400

    Add files via upload

---
+++ lab_6.py
@@ -0,0 +1,23 @@
+# 1
+def ejercicio_1():
+    for i in range (1,6):
+        for k in range(i):
+            print('*', end="")
+        print()
+
+#2
+def ejercicio_2():
+    for i in range (1,8):
+        for k in range(i):
+            print(i, end="")
+        print()
+
+#3
+def ejercicio_3():
+    for i in range (1,6):
+        for k in range(i):
+            print(i, end="")
+        print()
+
+
+ejercicio_3()
