6bf43c155c82266e076663d4e963e312253724c9
cisternna <105678565+cisternna@users.noreply.github.com>
2025-05-23 10:24:57 -0400

    Update lab_6.py

--- lab_6.py
+++ lab_6.py
@@ -14,10 +14,10 @@ def ejercicio_2():
 
 #3
 def ejercicio_3():
-    for i in range (1,6):
-        for k in range(i):
-            print(i, end="")
-        print()
+    for i in range(1, 6):  
+        for k in range(5 - i):  
+            print(" ", end="")
+        print(i) 
 
 
 ejercicio_3()
